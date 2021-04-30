442cb1b61951c0cabefab528f2042e9e442aa091
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-04-30 23:55:22 +0200

    поправив data файли

--- data/data.js
+++ data/data.js
@@ -1,37 +1,37 @@
 export default [
   {
     id: 2,
-    img: '../img/red.png',
+    img: './img/red.png',
     title: 'This Is A Very Second Title',
     description:
       '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
   },
   {
     id: 5,
-    img: '../img/yellow.png',
+    img: './img/yellow.png',
     title: 'This Is A Very Ops Title',
     description:
-      '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
+      '  Vorem ipsum dolvt consectetur adipisicing elit. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
   },
   {
     id: 3,
-    img: '../img/gray.png',
+    img: './img/gray.png',
     title: 'This Is A Very Third Title',
     description:
-      '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
+      '  TTrem ipsum dolor sit amet celit. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
   },
   {
     id: 1,
-    img: '../img/black.png',
+    img: './img/black.png',
     title: 'This Is A Very Long Slider Title',
     description:
-      '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
+      '  Me ipsum dolor. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
   },
   {
     id: 4,
-    img: '../img/orange.png',
+    img: './img/orange.png',
     title: 'This Is A Very Next Title',
     description:
-      '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima a veritatis eum atque veritatis eum atque mollitia nisi quaerat animi!',
+      '  Orem consectetur doloradipisicing elit. Minima a veritatis eum atque veritatis eum atqueipsum dolor sit amet  mollitia nisi quaerat animi!',
   },
 ];
